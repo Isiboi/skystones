6b4ba2ad34a4ee1483c35b73cb1a135fa711fc11
Monk? <39006663+Isiboi@users.noreply.github.com>
2026-09-14 18:17:24 +0200

    Add files via upload

--- cards.js
+++ cards.js
@@ -30,11 +30,25 @@ const card_database = [
         right: 1,
         img: 'pikachu.png'
     }, {
-        name: 'Fearow',
+        name: 'Raichu',
+        top: 1,
+        bottom: 2,
+        left: 2,
+        right: 2,
+        img: 'raichu.png'
+    }, {
+        name: 'Spearow',
         top: "",
         bottom: 1,
         left: 1,
         right: "",
+        img: 'spearow.png'
+    }, {
+        name: 'Fearow',
+        top: 1,
+        bottom: 2,
+        left: 2,
+        right: 1,
         img: 'fearow.png'
     }, {
         name: 'Pidgey',
@@ -44,14 +58,34 @@ const card_database = [
         right: "",
         img: 'pidgey.png'
     }, {
+        name: 'Pidgeotto',
+        top: "",
+        bottom: 2,
+        left: 2,
+        right: "",
+        img: 'pidgeotto.png'
+    }, {
+        name: 'Pidgeot',
+        top: "",
+        bottom: 3,
+        left: 3,
+        right: "",
+        img: 'pidgeot.png'
+    }, {
         name: 'Ratata',
         top: "",
         bottom: 2,
         left: "",
         right: "",
         img: 'ratata.png'
-    }
-    , {
+    }, {
+        name: 'Raticate',
+        top: 1,
+        bottom: 3,
+        left: 1,
+        right: 1,
+        img: 'raticate.png'
+    }, {
         name: 'Ivysaur',
         top: 2,
         bottom: "",
@@ -74,6 +108,29 @@ const card_database = [
         left: "",
         right: "",
         img: 'wartortle.png'
+    }, {
+        name: 'Venosaur',
+        top: 3,
+        bottom: "",
+        left: "",
+        right: 3,
+        img: 'venosaur.png'
+    },
+    {
+        name: 'Charizard',
+        top: 3,
+        bottom: "",
+        left: 3,
+        right: "",
+        img: 'charizard.png'
+    },
+    {
+        name: 'Blastoise',
+        top: 3,
+        bottom: 3,
+        left: "",
+        right: "",
+        img: 'blastoise.png'
     }
 ]
 
